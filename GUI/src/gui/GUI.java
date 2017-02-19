697b9df3b1d6544fd7c9a8d4df1b8be61286fe18
phillip <phillipmcknigh@gmail.com>
2017-02-19 18:07:38 -0500

    Created a basic window that spans the entire touch screen

---
+++ GUI/src/gui/GUI.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author phillip
+ */
+public class GUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame frame = new JFrame("Eastman Screw Convayor Controls");
+        frame.setSize(800, 480);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
