177c131d00b7223dbdfc6fa9b16261689bd510cf
Nicholas DeJulia <ndejulia@vols.utk.edu>
2017-03-21 09:33:17 -0400

    Initial Upload
    Initial Upload, not confirmed to be working or anything, just something to get up. Manual mode not yet implemented.
    
    Just using for statements and such until a better solution is found. Currently investigation Newton's Law of Cooling and such. Dr. Simpson ,(math professor in DEs and PDEs) said for what I've described it's gonna more like for statements.
    
    Currently checks when temp is within +/-5 degrees of goal, and adjusts accordingly. Has wait times which will need to be adjusted for water propagation. 
    
    Not tested on actual Raspberry Pi with GPIO's so I do not know if all variables are set accordingly.

--- Main/mainprogram.py
+++ Main/mainprogram.py
@@ -46,33 +46,33 @@ while (Manual = 0):
 
 
 #Start while loop for manual mode, need variables and such, WIP.
-while (Manual = 0):
+while (Manual = 1):
         #Setup temp variables
         #Screw temp1,2 in the system, Input is at start, Output is at end, Serial is mixture of cold/city
-        screwtemp1 = temp.read_serial_screw_1()
-        initial = temp.read_serial_input()
-        screwtemp2 = temp.read_serial_screw_2()
-        final = temp.read_serial_output()
-        mixture = temp.read_serial_water()
+        #screwtemp1 = temp.read_serial_screw_1()
+        #initial = temp.read_serial_input()
+        #screwtemp2 = temp.read_serial_screw_2()
+        #final = temp.read_serial_output()
+        #mixture = temp.read_serial_water()
 
         #Motor Control, (frequency, direction, enabled)
-        motor.motor_control(800,0,1)
+        #motor.motor_control(800,0,1)
 
         #Work with temperature and opening valve
-        if(final > 100 && final <= 105):
+        #if(final > 100 && final <= 105):
                 #Do nothing
-        if(final > 105):
+       # if(final > 105):
                 #open cold valve
-                valve.open_cold()
-                time.sleep(2)
-                valve.stop()
-        if(final < 100 && final >= 90):
+        #        valve.open_cold()
+         #       time.sleep(2)
+          #      valve.stop()
+        #if(final < 100 && final >= 90):
                 #Do nothing
-        if(final < 90):
+      #  if(final < 90):
                 #open city valve
-                valve.open_hot()
-                time.sleep(2)
-                valve.stop()
+       #         valve.open_hot()
+        #        time.sleep(2)
+         #       valve.stop()
 
         #Propagation for water to hit end of system
-        time.sleep(4)
+       # time.sleep(4)
