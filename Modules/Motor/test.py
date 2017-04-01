ac459df41ad871c862b37b0fa7c061532434aadc
vkalaria <vasu.kalaria@gmail.com>
2017-04-01 00:19:28 +0000

    Add files via upload
    motor2.py is working version. Getting clean output. Use test2.py to test it.

---
+++ Modules/Motor/test.py
@@ -0,0 +1,7 @@
+import os
+import time
+
+import motor
+
+while (True):
+    motor.motor_control(10, 0)
--- Modules/Motor/test2.py
+++ Modules/Motor/test2.py
@@ -1,29 +1,9 @@
 import os
+import glob
 import time
 
-import motor
+import motor2
 
 while (True):
     
-    motor.motor_control(800, 10, 1)
-    time.sleep(8)
-    
-    motor.motor_control(800, 25, 1)
-    time.sleep(8)
-    
-    motor.motor_control(800, 50, 1)
-    time.sleep(8)
-    
-    motor.motor_control(800, 75, 1)
-    time.sleep(8)
-    
-    motor.motor_control(800, 90, 1)
-    time.sleep(8)
-    
-    motor.motor_control(800, 75, 0)
-    time.sleep(8)
-    
-    motor.motor_control(800, 90, 0)
-    time.sleep(8)
-    
-    motor.stop()
+    motor2.motor_control(4000, 1, 1)
