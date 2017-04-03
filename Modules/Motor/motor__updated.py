547c8d924ad3a2d3c51c3d2ff5f79a118cf1dc53
Vasu Kalaria <vasu.kalaria@gmail.com>
2017-04-03 09:50:59 -0400

    Update and rename motor1.py to motor__updated.py

--- Modules/Motor/motor__updated.py
+++ Modules/Motor/motor__updated.py
@@ -4,15 +4,16 @@ import time
 
 import RPi.GPIO as gpio
 
-PULSE_PIN     = 11
-DIRECTION_PIN = 12
-ENABLE_PIN    = 13
+PULSE_PIN     = 11  #GPIO17
+DIRECTION_PIN = 12  #GPIO18
+ENABLE_PIN    = 13  #GPIO27
 
 # Use board pin numbers
 gpio.setmode(gpio.BOARD)
 
 # Pin for pulse signal
 gpio.setup(PULSE_PIN, gpio.OUT)
+gpio.output(PULSE_PIN, gpio.LOW)
 
 # Pin for direction signal
 gpio.setup(DIRECTION_PIN, gpio.OUT)
@@ -27,7 +28,7 @@ def motor_clockwise():
 
 def motor_stop():
     gpio.output(PULSE_PIN, gpio.LOW)
-    gpio.output(DIRECTION_PIN, gpio.LOW)
+#    gpio.output(DIRECTION_PIN, gpio.LOW)
     
 def motor_counter_clockwise():
     gpio.output(DIRECTION_PIN, gpio.HIGH)
@@ -39,7 +40,8 @@ def motor_counter_clockwise():
 #             1 for counterclockwise
 # enable:     set 0 to turn motor off
 def motor_control(freq, direction, enable=1):
-    clock = (1/freq) / 2
+    period = 1/float(freq)
+    clock  = period / float(2)
 
     if (enable):
         if (direction):
