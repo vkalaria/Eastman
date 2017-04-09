d7fdadc4347d8af8e2c9a4584a279c84a0020f28
Nicholas DeJulia <ndejulia@vols.utk.edu>
2017-04-09 18:10:47 -0400

    Main Program
    Beta Upload 1.0
    - Have not compiled on testing machine.

--- Main/mainprogram.py
+++ Main/mainprogram.py
@@ -3,76 +3,131 @@ import time
 import temp
 import valve
 import motor
+import socket
 
-#Still looking for better way, maybe using Newton's Law of Cooling
-#Not tested yet
+#Globals
+Start = 1
+Auto = 1
+frequency = 800
+Direction = 1
 
-#Temp variables
-Manual = 0;
-start = 200; #Not set, around what we might get, just for testing
-end = 100; #Not set, around what we want for final temp, just for testing
+def GetInfo():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect('localhost', 32415)
+	Socketinfo = s.recv(1024).decode()
+	s.close()
+	return Socketinfo
 
-#Start while loop for automatic mode
-while (Manual = 0):
-        #Setup temp variables
-        #Screw temp1,2 in the system, Input is at start, Output is at end, Serial is mixture of cold/city
-        screwtemp1 = temp.read_serial_screw_1()
-        initial = temp.read_serial_input()
-        screwtemp2 = temp.read_serial_screw_2()
-        final = temp.read_serial_output()
-        mixture = temp.read_serial_water()
+def HeatTransfer(ScrewStart, ScrewEnd, MaterialIn, MaterialOut, WMixture, frequency):
+	Xspecific = 0.2388 #Specific Heat of Xanthan Gum
+	Cspecific = 1.003 #Specific Heat of Water
+	MassFlow = 1368 #Flow Rate, lb/hr
+	
+	#Heat Equation calculations
+	Qdot = (MassFlow)*(Xspecific)*(MaterialIn - MaterialOut)
+	#Add initial water + new water, this is the transfer of hot water from material to water.
+	FinalWaterTemp = Qdot/(MassFlow*Cspecific)
 
-        #Motor Control, (frequency, direction, enabled)
-        motor.motor_control(800,0,1)
+	if(ScrewEnd > ScrewEnd + FinalWaterTemp + 5):
+		valve.openCold()
+		time.wait(2)
+		valve.stop()
+	if(ScrewEnd < ScrewEnd + FinalWaterTemp - 5):
+		valve.openHot()
+		time.wait(2)
+		valve.stop()
+	
+	#Read From Temp Sensors
+	ScrewStart = temp.read_serial_screw_1()
+	ScrewEnd = temp.read_serial_screw_2()
+	MaterialIn = temp.read_serial_input()
+	MaterialOut = temp.read_serial_output()
+	WMixture = temp.read_serial_water()
 
-        #Work with temperature and opening valve
-        if(final > 100 && final <= 105):
-                #Do nothing
-        if(final > 105):
-                #open cold valve
-                valve.open_cold()
-                time.sleep(2)
-                valve.stop()
-        if(final < 100 && final >= 90):
-                #Do nothing
-        if(final < 90):
-                #open city valve
-                valve.open_hot()
-                time.sleep(2)
-                valve.stop()
+		
 
-        #Propagation for water to hit end of system
-        time.sleep(4)
+	SendData = "tempStart:"
+        SendData += "{:.9f}".format(MaterialIn)
+        SendData += ','
+        SendData += "tempEnd:"
+        SendData += "{:.9f)".format(MaterialOut)
+        SendData += ','
+        SendData += "WaterTemp:"
+        SendData += "{:.9f)".format(FinalWater)
+        SendData += ','
+        SendData += "speed:"
+        SendData += str(frequency)
+        SendData += '\n'
+        SendInfo(SendData)
+	return 
+
+def SendInfo(SendData):
+	#Send info to netcode
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect('localhost', 32415)
+	s.send(SendData.encode())
+	s.close()
+	return
+def Main():
+	#Start the program
+	while(1):
+		#Grab info from GUI
+		GUI = GetInfo()
+
+		if(GUI == "Start"):
+			Start = 1
+		if(GUI == "Stop"):
+			Start = 0
+		if(GUI == "Auto"):
+			Auto = 1
+			frequency = 800
+			Direction = 1
+		if(GUI == "Manual"):
+			Auto = 0
+
+		#if Start is enabled
+		if(Start == 1):
+			#Manual Mode Variable Setting
+			if(Auto == 0):
+				if(GUI == "cold+"):
+					valve.openCold()
+					time.sleep(2)
+					valve.stop()
+				if(GUI == "tap+"):
+					valve.openHot()
+					time.sleep(2)
+					valve.stop()
+				if(GUI == "motor+"):
+					frequency = frequency + 5;
+					motor.motorControl(frequency, Direction, Start);
+				if(GUI == "motor-"):
+					frequency = frequency - 5;
+					motor.motorControl(frequency, Direction, Start);
 
 
-#Start while loop for manual mode, need variables and such, WIP.
-while (Manual = 1):
-        #Setup temp variables
-        #Screw temp1,2 in the system, Input is at start, Output is at end, Serial is mixture of cold/city
-        #screwtemp1 = temp.read_serial_screw_1()
-        #initial = temp.read_serial_input()
-        #screwtemp2 = temp.read_serial_screw_2()
-        #final = temp.read_serial_output()
-        #mixture = temp.read_serial_water()
+			#Read From Temp Sensors
+			ScrewStart = temp.read_serial_screw_1()
+			ScrewEnd = temp.read_serial_screw_2()
+			MaterialIn = temp.read_serial_input()
+			MaterialOut = temp.read_serial_output()
+			WMixture = temp.read_serial_water()
 
-        #Motor Control, (frequency, direction, enabled)
-        #motor.motor_control(800,0,1)
+			if(Auto == 1):
+				#Heat Transfer
+				HeatTransfer(ScrewStart, ScrewEnd, MaterialIn, MaterialOut, WMixture, frequency)
 
-        #Work with temperature and opening valve
-        #if(final > 100 && final <= 105):
-                #Do nothing
-       # if(final > 105):
-                #open cold valve
-        #        valve.open_cold()
-         #       time.sleep(2)
-          #      valve.stop()
-        #if(final < 100 && final >= 90):
-                #Do nothing
-      #  if(final < 90):
-                #open city valve
-       #         valve.open_hot()
-        #        time.sleep(2)
-         #       valve.stop()
+			if(Auto == 0):
+				SendData = "tempStart:"
+				SendData += "{:.9f}".format(MaterialIn)
+				SendData += ','
+				SendData += "tempEnd:"
+				SendData += "{:.9f)".format(MaterialOut)
+				SendData += ','
+				SendData += "WaterTemp:"
+				SendData += "{:.9f)".format(WMixture)
+				SendData += ','
+				SendData += "speed:"
+				SendData += str(frequency)
+				SendData += '\n'
+				SendInfo(SendData)
 
-        #Propagation for water to hit end of system
-       # time.sleep(4)
